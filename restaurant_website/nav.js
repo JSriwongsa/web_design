1733aff42625df5bd1167bf46f4f76ce06c5d42c
JSriwongsa <sriwongsa.jtp@gmail.com>
2023-05-31 16:21:39 -0500

    removed non working navbar

--- restaurant_website/nav.js
+++
@@ -1,8 +0,0 @@
-fetch('http://127.0.0.1:8000/nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
