e065db39f15ca1756ebf0753fa0339c199d063e9
JSriwongsa <sriwongsa.jtp@gmail.com>
2023-06-07 16:53:29 -0500

    added items.js to be use in order.js

---
+++ restaurant_website/items.js
@@ -0,0 +1,571 @@
+const fooditem = [
+    {
+        id:1,
+        name: 'Edamame',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:6.00,
+        img:'images/order/1.jpg',
+        quantity:1
+    },
+    {
+        id:2,
+        name: 'Spicy Edamame',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:6.00,
+        img:'images/order/2.jpg',
+        quantity:1
+    },
+    {
+        id:3,
+        name: 'Gyoza',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/3.jpg',
+        quantity:1
+    },
+    {
+        id:4,
+        name: 'Takoyaki',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:6.99,
+        img:'images/order/4.jpg',
+        quantity:1
+    },
+    {
+        id:5,
+        name: 'Ebi Okonomiyaki',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/5.jpg',
+        quantity:1
+    },
+    {
+        id:6,
+        name: 'Sake Carpaccio',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:12.99,
+        img:'images/order/6.jpg',
+        quantity:1
+    },
+    {
+        id:7,
+        name: 'Hamachi Carpaccio',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/7.jpg',
+        quantity:1
+    },
+    {
+        id:8,
+        name: 'Soft Shell Crab',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:12.99,
+        img:'images/order/8.jpg',
+        quantity:1
+    },
+    {
+        id:9,
+        name: 'Shrimp Tempura',
+        category: 'Appetizers',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/9.jpg',
+        quantity:1
+    },
+    {
+        id:10,
+        name: 'Miso Soup',
+        category: 'Soup',
+        rating: 4.3,
+        price:3.99,
+        img:'images/order/10.jpg',
+        quantity:1
+    },
+    {
+        id:11,
+        name: 'Spicy Miso Soup',
+        category: 'Soup',
+        rating: 4.3,
+        price:3.99,
+        img:'images/order/11.jpg',
+        quantity:1
+    },
+    {
+        id:12,
+        name: 'House Salad',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:6.99,
+        img:'images/order/12.jpg',
+        quantity:1
+    },
+    {
+        id:13,
+        name: 'Seaweed Salad',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/13.jpg',
+        quantity:1
+    },
+    {
+        id:14,
+        name: 'Cucumber Salad',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:5.99,
+        img:'images/order/14.jpg',
+        quantity:1
+    },
+    {
+        id:15,
+        name: 'Tako Su',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/15.jpg',
+        quantity:1
+    },
+    {
+        id:16,
+        name: 'Ebi Su',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/16.jpg',
+        quantity:1
+    },
+    {
+        id:17,
+        name: 'Mixed Sunomono',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:11.99,
+        img:'images/order/17.jpg',
+        quantity:1
+    },
+    {
+        id:18,
+        name: 'Spicy maguro poke Salad',
+        category: 'Sunomono Salad',
+        rating: 4.3,
+        price:12.99,
+        img:'images/order/18.jpg',
+        quantity:1
+    },
+    {
+        id:19,
+        name: 'Sake/Smoked Sake',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/19.jpg',
+        quantity:1
+    },
+    {
+        id:20,
+        name: 'Maguro/Shiro Maguro',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/20.jpg',
+        quantity:1
+    },
+    {
+        id:21,
+        name: 'Hamachi',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/21.jpg',
+        quantity:1
+    },
+    {
+        id:22,
+        name: 'Saba',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/22.jpg',
+        quantity:1
+    },
+    {
+        id:23,
+        name: 'Ebi',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/23.jpg',
+        quantity:1
+    },
+    {
+        id:24,
+        name: 'Hotate',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/24.jpg',
+        quantity:1
+    },
+    {
+        id:25,
+        name: 'Kani Kama',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/25.jpeg',
+        quantity:1
+    },
+    {
+        id:26,
+        name: 'Unagi',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/26.jpg',
+        quantity:1
+    },
+    {
+        id:27,
+        name: 'Ikura',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/27.jpg',
+        quantity:1
+    },
+    {
+        id:28,
+        name: 'Masago',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/28.jpg',
+        quantity:1
+    },
+    {
+        id:29,
+        name: 'Tamago',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:7.99,
+        img:'images/order/29.jpg',
+        quantity:1
+    },
+    {
+        id:30,
+        name: 'Wagyu Nigiri',
+        category: 'Nigiri/Sashimi',
+        rating: 4.3,
+        price:12.99,
+        img:'images/order/30.jpeg',
+        quantity:1
+    },
+    {
+        id:31,
+        name: 'Sushi A',
+        category: 'Sushi/Sashimi Platters',
+        rating: 4.3,
+        price:19.99,
+        img:'images/order/31.jpg',
+        quantity:1
+    },
+    {
+        id:32,
+        name: 'Sushi B',
+        category: 'Sushi/Sashimi Platters',
+        rating: 4.3,
+        price:23.99,
+        img:'images/order/31.jpg',
+        quantity:1
+    },
+    {
+        id:33,
+        name: 'Sashimi A',
+        category: 'Sushi/Sashimi Platters',
+        rating: 4.3,
+        price:35.99,
+        img:'images/order/31.jpg',
+        quantity:1
+    },
+    {
+        id:34,
+        name: 'Sashimi B',
+        category: 'Sushi/Sashimi Platters',
+        rating: 4.3,
+        price:39.99,
+        img:'images/order/31.jpg',
+        quantity:1
+    },
+    {
+        id:35,
+        name: 'Super Sashimi',
+        category: 'Sushi/Sashimi Platters',
+        rating: 4.3,
+        price:45.99,
+        img:'images/order/31.jpg',
+        quantity:1
+    },
+    {
+        id:36,
+        name: 'Tekka',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/32.jpg',
+        quantity:1
+    },
+    {
+        id:37,
+        name: 'Sake',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/33.jpg',
+        quantity:1
+    },
+    {
+        id:38,
+        name: 'Negi Hamachi',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/34.jpg',
+        quantity:1
+    },
+    {
+        id:39,
+        name: 'Ebi',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/35.jpg',
+        quantity:1
+    },
+    {
+        id:40,
+        name: 'Sake Avocado',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/36.jpg',
+        quantity:1
+    },
+    {
+        id:41,
+        name: 'Unagi Q',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/37.jpg',
+        quantity:1
+    },
+    {
+        id:42,
+        name: 'Spicy Scallop',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/38.jpg',
+        quantity:1
+    },
+    {
+        id:43,
+        name: 'Spicy Tako',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:9.99,
+        img:'images/order/39.jpg',
+        quantity:1
+    },
+    {
+        id:44,
+        name: 'Spider',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:12.99,
+        img:'images/order/40.jpg',
+        quantity:1
+    },
+    {
+        id:45,
+        name: 'Alaska',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/41.jpg',
+        quantity:1
+    },
+    {
+        id:46,
+        name: 'Boston',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/42.jpg',
+        quantity:1
+    },
+    {
+        id:47,
+        name: 'Philadelphia',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/43.jpg',
+        quantity:1
+    },
+    {
+        id:48,
+        name: 'California',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/44.jpg',
+        quantity:1
+    },
+    {
+        id:49,
+        name: 'Chicago',
+        category: 'Maki (Rolls)',
+        rating: 4.3,
+        price:13.99,
+        img:'images/order/45.jpg',
+        quantity:1
+    },
+    {
+        id:50,
+        name: 'Sake Donburi',
+        category: 'Donburi',
+        rating: 4.3,
+        price:16.99,
+        img:'images/order/46.jpg',
+        quantity:1
+    },
+    {
+        id:51,
+        name: 'Tekka Donburi',
+        category: 'Donburi',
+        rating: 4.3,
+        price:17.99,
+        img:'images/order/47.jpg',
+        quantity:1
+    },
+    {
+        id:52,
+        name: 'Hamachi Donburi',
+        category: 'Donburi',
+        rating: 4.3,
+        price:17.99,
+        img:'images/order/48.jpg',
+        quantity:1
+    },
+    {
+        id:53,
+        name: 'Chirashi Donburi',
+        category: 'Donburi',
+        rating: 4.3,
+        price:19.99,
+        img:'images/order/49.jpg',
+        quantity:1
+    },
+    {
+        id:54,
+        name: 'Unagi Donburi',
+        category: 'Donburi',
+        rating: 4.3,
+        price:19.99,
+        img:'images/order/50.jpg',
+        quantity:1
+    },
+    {
+        id:55,
+        name: 'Hot Green Tea',
+        category: 'Beverages',
+        rating: 4.3,
+        price:2.99,
+        img:'images/order/51.jpg',
+        quantity:1
+    },
+    {
+        id:56,
+        name: 'Iced Green Tea',
+        category: 'Beverages',
+        rating: 4.3,
+        price:3.99,
+        img:'images/order/52.jpg',
+        quantity:1
+    },
+    {
+        id:57,
+        name: 'Coke/Diet',
+        category: 'Beverages',
+        rating: 4.3,
+        price:2.59,
+        img:'images/order/53.png',
+        quantity:1
+    },
+    {
+        id:58,
+        name: 'Bottled Water',
+        category: 'Beverages',
+        rating: 4.3,
+        price:1.99,
+        img:'images/order/54.png',
+        quantity:1
+    },
+    {
+        id:59,
+        name: 'Matcha Ice Cream',
+        category: 'Desserts',
+        rating: 4.3,
+        price:5.99,
+        img:'images/order/55.jpg',
+        quantity:1
+    },
+    {
+        id:60,
+        name: 'Mochi Ice Cream',
+        category: 'Desserts',
+        rating: 4.3,
+        price:4.99,
+        img:'images/order/56.jpg',
+        quantity:1
+    },
+    {
+        id:61,
+        name: 'Matcha Cheesecake',
+        category: 'Desserts',
+        rating: 4.3,
+        price:8.99,
+        img:'images/order/57.jpg',
+        quantity:1
+    },
+    {
+        id:62,
+        name: 'Dango',
+        category: 'Desserts',
+        rating: 4.3,
+        price:3.99,
+        img:'images/order/58.jpg',
+        quantity:1
+    },
+    {
+        id:63,
+        name: 'Taiyaki',
+        category: 'Desserts',
+        rating: 4.3,
+        price:6.99,
+        img:'images/order/59.jpg',
+        quantity:1
+    },
+]
+
+export {fooditem};
