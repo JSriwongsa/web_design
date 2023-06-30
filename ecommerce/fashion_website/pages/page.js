c035304c72e727fd265e7ce4ebeaa1a067a4e5ff
JSriwongsa <sriwongsa.jtp@gmail.com>
2023-06-30 18:48:28 -0500

    added page section

---
+++ ecommerce/fashion_website/pages/page.js
@@ -0,0 +1,30 @@
+const openBtn = document.querySelectorAll('[trigger-button]');
+const closeBtn = document.querySelectorAll('[close-button]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let x = 0; x < openBtn.length; x++){
+    let currentID = openBtn[x].dataset.target,
+    targetEl =document.querySelector(`#${currentID}`)
+
+    const openData =function(){
+        targetEl.classList.remove('active');
+        overlay.classList.remove('active');
+    };
+    openBtn[x].addEventListener('click', function(){
+        targetEl.classList.add('active');
+        overlay.classList.add('active');
+    });
+    targetEl.querySelector('[close-button]').addEventListener('click',openData);
+    overlay.addEventListener('click', openData);
+}
+
+
+const subMenu = document.querySelectorAll('.child-trigger');
+    subMenu.forEach((menu) => menu.addEventListener('click', function(e){
+        e.preventDefault();
+        subMenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
+
+        if(this.closest('.has-child').classList != 'active'){
+            this.closest('.has-child').classList.toggle('active');
+        }
+    }))
